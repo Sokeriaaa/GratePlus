9149ed1093251c4e3328c2755b8341264fa7b3ef
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-06-21 12:35:33 +0800

    ✨ Register blocks.

--- src/main/kotlin/sokeriaaa/grateplus/GratePlus.kt
+++ src/main/kotlin/sokeriaaa/grateplus/GratePlus.kt
@@ -1,13 +1,14 @@
 package sokeriaaa.grateplus
 
 import net.minecraft.resources.ResourceLocation
+import sokeriaaa.grateplus.registry.ModBlocks
 
 object GratePlus {
     const val MOD_ID: String = "grate_plus"
 
     @JvmStatic
     fun init() {
-
+        ModBlocks.register()
     }
 
     fun id(path: String): ResourceLocation =
---
+++ src/main/kotlin/sokeriaaa/grateplus/registry/ModBlocks.kt
@@ -0,0 +1,81 @@
+package sokeriaaa.grateplus.registry
+
+import net.minecraft.world.item.CreativeModeTabs
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.WaterloggedTransparentBlock
+import net.minecraft.world.level.block.state.BlockBehaviour
+import sokeriaaa.grateplus.platform.Platform
+import sokeriaaa.grateplus.platform.registry.RegistryObject
+
+object ModBlocks {
+
+    val AMETHYST_GRATE = block("amethyst_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val DIAMOND_GRATE = block("diamond_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val EMERALD_GRATE = block("emerald_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val GOLD_GRATE = block("gold_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val IRON_GRATE = block("iron_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val LAPIS_GRATE = block("lapis_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val NETHERITE_GRATE = block("netherite_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    val REDSTONE_GRATE = block("redstone_grate") {
+        WaterloggedTransparentBlock(
+            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+        )
+    }
+
+    fun register() {
+        Platform.registry.apply {
+            registerBlockAndItem(REDSTONE_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(GOLD_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(IRON_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(EMERALD_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(DIAMOND_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(LAPIS_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(NETHERITE_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+            registerBlockAndItem(AMETHYST_GRATE, CreativeModeTabs.BUILDING_BLOCKS)
+        }
+    }
+
+    private inline fun <reified T : Block> block(
+        path: String,
+        noinline supplier: () -> T,
+    ) = RegistryObject(
+        path = path,
+        lazy = lazy(supplier),
+    )
+}
