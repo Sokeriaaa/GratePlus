a5728489a0dbb4405f88cb40572a7c2374c48e56
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-06-21 14:26:42 +0800

    ✨ Change block properties.

--- src/main/kotlin/sokeriaaa/grateplus/registry/ModBlocks.kt
+++ src/main/kotlin/sokeriaaa/grateplus/registry/ModBlocks.kt
@@ -3,6 +3,7 @@ package sokeriaaa.grateplus.registry
 import net.minecraft.world.item.CreativeModeTabs
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.WaterloggedTransparentBlock
 import net.minecraft.world.level.block.state.BlockBehaviour
 import sokeriaaa.grateplus.platform.Platform
@@ -12,49 +13,49 @@ object ModBlocks {
 
     val AMETHYST_GRATE = block("amethyst_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.AMETHYST_BLOCK).asGrate()
         )
     }
 
     val DIAMOND_GRATE = block("diamond_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_BLOCK).asGrate()
         )
     }
 
     val EMERALD_GRATE = block("emerald_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.EMERALD_BLOCK).asGrate()
         )
     }
 
     val GOLD_GRATE = block("gold_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.GOLD_BLOCK).asGrate()
         )
     }
 
     val IRON_GRATE = block("iron_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).asGrate()
         )
     }
 
     val LAPIS_GRATE = block("lapis_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.LAPIS_BLOCK).asGrate()
         )
     }
 
     val NETHERITE_GRATE = block("netherite_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK).asGrate()
         )
     }
 
     val REDSTONE_GRATE = block("redstone_grate") {
         WaterloggedTransparentBlock(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COPPER_GRATE)
+            BlockBehaviour.Properties.ofFullCopy(Blocks.REDSTONE_BLOCK).asGrate()
         )
     }
 
@@ -69,6 +70,15 @@ object ModBlocks {
             NETHERITE_GRATE.value,
             REDSTONE_GRATE.value,
         )
+
+    private fun BlockBehaviour.Properties.asGrate(): BlockBehaviour.Properties =
+        this.sound(SoundType.COPPER_GRATE)
+            .noOcclusion()
+            .requiresCorrectToolForDrops()
+            .isValidSpawn { _, _, _, _ -> false }
+            .isRedstoneConductor { _, _, _ -> false }
+            .isSuffocating { _, _, _ -> false }
+            .isViewBlocking { _, _, _ -> false }
 
     fun register() {
         Platform.registry.apply {
